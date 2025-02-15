192a2e3e33677be70bbe94b7896dcb36ca98d5ba
MdEmon1346 <emamulhoque1346@gmail.com>
2025-02-15 12:04:09 +0600

    second-commit

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-'use strict';
-/*console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🍕 Correct Number';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent= 10;
-
-document.querySelector('.guess').value=23;
-*/
 let SecNum = Math.trunc(Math.random() * 20 + 1);
 let highscore =0;
 let score = 20;
